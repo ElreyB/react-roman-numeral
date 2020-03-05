c833d8fc688166daf2481250e9b7bdfca05481c0
Elrey Belmonti <elbelmonti@gmail.com>
2020-03-05 12:47:57 -0800

    refactor Form tests

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -6,14 +6,17 @@ describe("Form", () => {
   it("render Form without crashing", () => {
     expect(() => render(<Form converter="roman" />)).not.toThrow();
   });
-
-  it("should render 'Roman Numeral Converter' as title", () => {
-    const { queryByText } = render(<Form converter="roman" />);
-    expect(queryByText("Roman Numeral Converter")).not.toBeNull();
+  describe("converter prop is roman", () => {
+    it("should render 'Roman Numeral Converter' as title", () => {
+      const { queryByText } = render(<Form converter="roman" />);
+      expect(queryByText("Roman Numeral Converter")).not.toBeNull();
+    });
   });
 
-  it("should render 'Arabic Number Converter' as title", () => {
-    const { queryByText } = render(<Form converter="arabic" />);
-    expect(queryByText("Arabic Number Converter")).not.toBeNull();
+  describe("converter prop is arabic", () => {
+    it("should render 'Arabic Number Converter' as title", () => {
+      const { queryByText } = render(<Form converter="arabic" />);
+      expect(queryByText("Arabic Number Converter")).not.toBeNull();
+    });
   });
 });
